d5845f8755ee8c9d058ab6d89668f87f4d7f6b97
Maxim Rumyantsev <maxrumyant@mail.ru>
2021-10-21 18:02:04 +0300

    update add

--- add.c
+++ add.c
@@ -10,14 +10,26 @@ int strlength(const char* str)
 }
 char* find(const char* str, const char* substr)
 {
-	char* t;
+	int len = strlength(substr);
 	int flag = 0;
+	_Bool value = 0;
 	while (*(str + flag) != 0)
 	{
 		if (*(str + flag) == *substr)
 		{
-
-			return (str + flag);
+			int i = 0;
+			while (i < len)
+			{
+				if (*(str + flag + i) != *(substr + i) && *(str + flag + i) != 0) {
+					value = 1;
+					break;
+				}
+				i++;
+			}
+			if (value) { break; }
+			else {
+				return (str + flag);
+			}
 		}
 		flag++;
 	}
